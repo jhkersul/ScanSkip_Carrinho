cf61b0dd72985605e08783c410869f1e146059e9
Giovanni Pigola <giovanni.pigola.mannucci@gmail.com>
2016-11-29 19:38:22 -0200

    Add files via upload

--- carrinho/Templates/js/mapa.js
+++ carrinho/Templates/js/mapa.js
@@ -7,6 +7,12 @@ $( document ).ready(function() {
     let idSetor = $(this).attr('id').replace("element-", "");
     selectElement([idSetor]);
     getProducts(idSetor);
+    $("#myModal").modal();
+  });
+  //tentativa de apagar os produtos antigos pra quando clicar no outro setor o modal não manter tudo e ficar empilhando
+  $("input#btClose").click(function(){
+    var modalBody = document.getElementsByClassName("modal-body");
+    $(".modal-body").val("");
   });
 
 });
@@ -34,8 +40,57 @@ function getProducts(idSetor) {
         console.log("Imagem: " + data[index]['imagem']);
         console.log("Marca: " + data[index]['marca']);
         console.log("Valor: " + data[index]['valor']);
+
+        var nomeModal = document.getElementsByClassName('modal-title');
+        nomeModal.innerHTML = data[index]['idProduto'];
+        var divProduto = document.createElement('div');
+        divProduto.id = index;
+        divProduto.className = 'produtocarrinho';
+        document.getElementsByClassName('modal-body')[0].appendChild(divProduto);
+
+        var divInfo = document.createElement('div');
+        divInfo.className = 'infocarrinho';
+        divProduto.appendChild(divInfo);
+
+        var infoNome = document.createElement('P');
+        infoNome.className = 'nomeproduto';
+        divInfo.appendChild(infoNome);
+        infoNome.innerHTML = data[index]['nome'];
+        
+        var infoMarca = document.createElement('P');
+        infoMarca.className = 'marcaproduto';
+        divInfo.appendChild(infoMarca);
+        infoMarca.innerHTML = data[index]['marca'];
+
+        var infoPreco = document.createElement('P');
+        infoPreco.className = 'precoproduto';
+        divInfo.appendChild(infoPreco);
+        infoPreco.innerHTML = data[index]['valor'];
+
+        var produtoFoto = document.createElement('div');
+        produtoFoto.className = 'fotoprodutocarrinho';
+        divProduto.appendChild(produtoFoto);
+
+        //tentativa de conversão do base64
+        var base64imgC = data[index]['imagem'];
+        console.log(base64imgC);
+
+        var base64img = base64imgC.split(',')[1];
+        console.log(base64img);
+
+        var urls = window.atob(base64img);
+        console.log(urls);
+
+        produtoFoto.style.backgroundImage = base64imgC;
+
+
+        //tentativa de mudar o titulo do modal pegando o nome da categoria que vem do PLU (mudado hoje)
+        var modalTitle = document.getElementsById("myModalLabel");
+        modalTitle.innerHTML = data[index]['categoria']
+
       }
     }
-  });
+  }
+  );
 
 }
